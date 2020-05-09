b27be176e0f6e47769e23c7ef63c0647c148faaf
Justin Bull <me@justinbull.ca>
2020-05-09 09:44:42 -0400

    Render basic OpenStreetMap setup with Leaflet

--- renderer.js
+++ renderer.js
@@ -4,3 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const map = L.map('map').setView([43.6532, -79.3832], 4);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  maxZoom: 9,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>'
+}).addTo(map);
